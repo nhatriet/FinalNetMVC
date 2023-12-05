c73ec1c401ca5df4a421ca78f374634a20dd242f
khapyeup <118350564+khapyeup@users.noreply.github.com>
2023-12-05 14:54:41 +0700

    Thêm bảng Thống kê

---
+++ Models/Statistic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EuphoriaShop.Models
+{
+    public partial class Statistic
+    {
+        public int StatId { get; set; }
+        public int Year { get; set; }
+        public int TotalProduct { get; set; }
+    }
+}
